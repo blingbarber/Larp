8e78389792d1a11723ad1e8e81cdf62aac06102e
barber <johntoeishere@gmail.com>
2026-09-09 15:54:05 -0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -31,15 +31,15 @@ local Library = {
 
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
-    BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(0, 85, 255);
-    OutlineColor = Color3.fromRGB(50, 50, 50);
+    BackgroundColor = Color3.fromRGB(22, 22, 22);
+    AccentColor = Color3.fromRGB(206, 206, 206);
+    OutlineColor = Color3.fromRGB(29, 29, 29);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
     Black = Color3.new(0, 0, 0);
 
     Font = Enum.Font.Code,
-    FontSize = 14,
+    FontSize = 13,
 
     OpenedFrames = {};
     DependencyBoxes = {};
